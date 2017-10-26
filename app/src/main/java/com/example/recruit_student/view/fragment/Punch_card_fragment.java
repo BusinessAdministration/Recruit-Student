ef985e292ebebd6bb66957149fd7a4240bb5f82c
ZhangqinglingA <17600920523@189.cn>
2017-10-26 09:30:22 +0800

    张庆龄网络判断效果

--- app/src/main/java/com/example/recruit_student/view/fragment/Punch_card_fragment.java
+++ app/src/main/java/com/example/recruit_student/view/fragment/Punch_card_fragment.java
@@ -1,9 +1,13 @@
 package com.example.recruit_student.view.fragment;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
@@ -21,7 +25,6 @@ import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.example.recruit_student.R;
-import com.example.recruit_student.utils.WangluoUtil;
 import com.muddzdev.styleabletoastlibrary.StyleableToast;
 
 /**
@@ -50,6 +53,8 @@ public class Punch_card_fragment extends Fragment implements View.OnClickListene
     private String weizhi;
     private TextView shezhi;
     private StyleableToast styleableToast;
+    private NetworkChangeReciver network;
+    private IntentFilter intentfile;
 
     @Nullable
     @Override
@@ -62,6 +67,11 @@ public class Punch_card_fragment extends Fragment implements View.OnClickListene
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        intentfile = new IntentFilter();
+        intentfile.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        network = new NetworkChangeReciver();
+        getActivity().registerReceiver(network, intentfile);
+
         setListener();
     }
 
@@ -89,15 +99,16 @@ public class Punch_card_fragment extends Fragment implements View.OnClickListene
             case R.id.gongzuo:
                     beizhu = ed_text.getText().toString().trim();
                 if (TextUtils.isEmpty(beizhu)){
-                    StyleableToast.makeText(getActivity(), "请输入打卡信息",Toast.LENGTH_LONG, R.style.StyleableToast).show();
+                    StyleableToast.makeText(getActivity(), "请输入打卡信息",Toast.LENGTH_SHORT, R.style.StyleableToast).show();
                }else{
                     StyleableToast.makeText(getActivity(), "您在"+weizhi+"地区工作打卡成功,备注"+beizhu,Toast.LENGTH_SHORT, R.style.StyleableToast).show();
                     ed_text.setText("");
                 }
                 break;
             case R.id.chuchai:
+                beizhu = ed_text.getText().toString().trim();
                 if (TextUtils.isEmpty(beizhu)) {
-                    StyleableToast.makeText(getActivity(), "请输入打卡信息",Toast.LENGTH_LONG, R.style.StyleableToast).show();
+                    StyleableToast.makeText(getActivity(), "请输入打卡信息",Toast.LENGTH_SHORT, R.style.StyleableToast).show();
                 }else {
                     StyleableToast.makeText(getActivity(), "您在"+weizhi+"地区出差打卡成功,备注"+beizhu,Toast.LENGTH_SHORT, R.style.StyleableToast).show();
                     ed_text.setText("");
@@ -107,29 +118,15 @@ public class Punch_card_fragment extends Fragment implements View.OnClickListene
         }
     }
 
-    // TODO: 2017/10/25 得到您当前的位置，判断网络
+    // TODO: 2017/10/25 得到您当前的位置
     public class MyLocationListenner implements BDLocationListener {
 
         @Override
         public void onReceiveLocation(BDLocation location) {
-            if (WangluoUtil.isConnection(getActivity())){
-                str = "您当前的位置：" + location.getAddress().address + ",纬度：" + location.getLatitude();
-                weizhi = location.getAddress().address;
-                dingwei.setText(str);
-
-            }else {
-                dingwei.setText("请打开网络完成定位");
-                shezhi.setText("网络连接错误，请您检查网络设置        >");
-                shezhi.setBackgroundColor(Color.parseColor("#e24747"));
-                shezhi.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
-                    }
-                });
+                    str = "您当前的位置：" + location.getAddress().address + ",纬度：" + location.getLatitude();
+                    weizhi = location.getAddress().address;
             }
 
-        }
     }
 
     // TODO: 2017/10/25 得到ID
@@ -144,5 +141,37 @@ public class Punch_card_fragment extends Fragment implements View.OnClickListene
         chuchai.setOnClickListener(this);
     }
 
+    // TODO: 2017/10/26 广播监听网络判断进行没网络与有网络时的页面设置
+    class NetworkChangeReciver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager connectivityManager=(ConnectivityManager)getActivity().getSystemService(context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo=connectivityManager.getActiveNetworkInfo();
+            if(networkInfo!=null&&networkInfo.isAvailable()){
+                dingwei.setText(str);
+                shezhi.setText(null);
+                shezhi.setBackgroundColor(0);
+                shezhi.setOnClickListener(null);
+
+            }else{
+                dingwei.setText("请打开网络完成定位");
+                shezhi.setText("网络连接错误，请您检查网络设置        >");
+                shezhi.setBackgroundColor(Color.parseColor("#e24747"));
+                shezhi.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_AIRPLANE_MODE_SETTINGS));
+                    }
+                });
+            }
+
+        }
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getActivity().unregisterReceiver(network);
+    }
 }
 
